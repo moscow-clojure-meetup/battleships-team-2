38a9c54529d05017789f8b2e06927d79ef808a1c
Alexander Ponimaskin <ponimaskin@gmail.com>
2019-03-24 18:37:23 +0300

    returning victory state

--- src/backend/core.cljs
+++ src/backend/core.cljs
@@ -3,15 +3,15 @@
    [bidi.bidi :as bidi]
    [macchiato.util.response :as r]
    [macchiato.server :as http]
-
    [mount.core :as mount :refer [defstate]]
-
    [macchiato.middleware.defaults :as defaults]
    [taoensso.timbre :refer-macros [log trace debug info warn error fatal]]))
 
+(def server-state (atom {}))
+(def avaiting-player (atom nil))
 
 (defn start [req res raise]
-  (r/ok {:a 1}))
+  (r/ok {:victory (< (rand 1) 0.7) }))
 
 (defn not-found [req res raise]
   (r/not-found {}))
@@ -28,7 +28,7 @@
    defaults/site-defaults))
 
 (defn main []
-  (let [host "127.0.0.1" port 3000]
+  (let [host "0.0.0.0" port 3000]
     (http/start
      {:handler    router
       :host       host
