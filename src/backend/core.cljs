73bfc0ef8419a6b34553b52fa29309c67b5a990d
Alexander Ponimaskin <ponimaskin@gmail.com>
2019-03-24 18:17:31 +0300

    hello backend

--- src/backend/core.cljs
+++ src/backend/core.cljs
@@ -3,34 +3,33 @@
    [bidi.bidi :as bidi]
    [macchiato.util.response :as r]
    [macchiato.server :as http]
+
    [mount.core :as mount :refer [defstate]]
-   [macchiato.middleware.restful-format :as rf]
+
+   [macchiato.middleware.defaults :as defaults]
    [taoensso.timbre :refer-macros [log trace debug info warn error fatal]]))
 
 
-(defn start [res req raise]
-  (r/ok {:one 1}))
+(defn start [req res raise]
+  (r/ok {:a 1}))
 
-(defn not-found [& _]
+(defn not-found [req res raise]
   (r/not-found {}))
-
 
 (def routes
   ["/start" {:get start}])
 
-
 (defn router [req res raise]
-  (if-let [{:keys [handler route-params]} (bidi/match-route* routes (:uri req) req)]
-    (handler (assoc req :route-params route-params) res raise)
-    (not-found req res raise)))
-
+  (defaults/wrap-defaults
+   (if-let [{:keys [handler route-params]} (bidi/match-route* routes (:uri req) req)]
+     (-> req (assoc :route-params route-params) (handler res raise))
+     (not-found req res raise))
+   defaults/site-defaults))
 
 (defn main []
-  (mount/start)
-  (let [host "0.0.0.0"
-        port 3000]
+  (let [host "127.0.0.1" port 3000]
     (http/start
-     {:handler    (rf/wrap-restful-format router)
+     {:handler    router
       :host       host
       :port       port
       :on-success #(info "test started on" host ":" port)})))
